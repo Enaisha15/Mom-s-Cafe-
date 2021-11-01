b37f3c5ac4f6f2738d539cce20d6376da306e871
Enaisha <Enaisha15@gmail.com>
2021-11-01 00:34:31 -0400

    Barista Project

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://Enaisha15:Jasmine123!@cluster0.jcysh.mongodb.net/barista?retryWrites=true&w=majority',
+    'dbName': 'orders'
+};
